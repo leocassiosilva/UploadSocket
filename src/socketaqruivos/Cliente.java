5027ee4ff80c5fe198fcaab335bda19153457007
leocassiosilva <leocassiosilva1234@gmail.com>
2020-10-20 08:31:58 -0300

    finalizando

--- src/socketaqruivos/Cliente.java
+++ src/socketaqruivos/Cliente.java
@@ -1,11 +1,12 @@
 package socketaqruivos;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFileChooser;
 
 /**
  *
@@ -15,19 +16,33 @@ public class Cliente {
 
     public static void main(String[] args) {
         try {
-            Socket conexao = new Socket("localhost", 5555);
-            System.out.println("Conexao efetuada");
-            conexao.close();
-            JFileChooser fileChooser = new JFileChooser();
-            int opt = fileChooser.showOpenDialog(null);
-            
-            if (opt == JFileChooser.APPROVE_OPTION) {
-                File file = fileChooser.getSelectedFile();
-                System.out.println(file);
+            Socket conexao = new Socket("localhost", 999); //estabelecer a conexao
+            System.out.println("Conectado!");
+
+            ObjectOutputStream out = new ObjectOutputStream(conexao.getOutputStream());//serve para enviar/escrevr 
+
+            File file = new File("C:\\z\\resolução-4k.jpg"); //colocar na variavel file o cainho e arquivo
+
+            FileInputStream fileInputStream = new FileInputStream(file);//ler os dados 
+
+            System.out.println(file.length()); //informa o tamanho do arquivo
+
+            byte[] array = new byte[4096]; //array de byte
+
+            while (true) {
+                int len = fileInputStream.read(array);//leitura 
+                if (len == -1) {
+                    break;
+                }
+                out.write(array, 0, len);//escrita
             }
+
+            System.out.println("Enviado para o servidor!"); 
+            //Finalizando
+            conexao.close();
+            fileInputStream.close();
         } catch (IOException ex) {
-            Logger.getLogger(Cliente.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
     }
 }
--- src/socketaqruivos/Servidor.java
+++ src/socketaqruivos/Servidor.java
@@ -1,6 +1,9 @@
 package socketaqruivos;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.logging.Level;
@@ -13,16 +16,33 @@ import java.util.logging.Logger;
 public class Servidor {
 
     public static void main(String[] args) {
-
         try {
-            ServerSocket servidor = new ServerSocket(5555);
-            Socket socket;
-
-            System.out.println("Servidor on!");
-            Socket conexao = servidor.accept();
-
-            System.out.println("Conexão efetuada " + conexao.getInetAddress().getHostAddress());
+            ServerSocket conexao = new ServerSocket(999); //Conexao
+            System.out.println("Aguardando conexão");
             
+            Socket servidor = conexao.accept(); //conexão aceita
+            System.out.println("Conexão efetuada " + servidor.getInetAddress().getHostAddress()); //Apresenta quando um cliente se conecta ao servidor
+            ObjectInputStream out = new ObjectInputStream(servidor.getInputStream());
+
+            File file = new File("C:\\Users\\Leocassio\\Documents\\Projects\\newOrder.jpg"); //Adiciona a variavel file informações do caminhos e nome do arquivo
+
+            FileOutputStream fileOutputStream = new FileOutputStream(file); //serve para escrever 
+
+            byte[] array = new byte[4096]; //array de byte
+
+            while (true) {//
+                int len = out.read(array); //serve para realizar a leitura das informações que estão sendo enviadas 
+                if (len == -1) {
+                    break;
+                }
+                fileOutputStream.write(array, 0, len); // essa parte é responsavel pela escritado do arquivo na parte do servidor
+            }
+            
+            //Aqui estão sendo finalizadas
+            conexao.close();
+            servidor.close();
+            out.close();
+            fileOutputStream.close();
         } catch (IOException ex) {
             Logger.getLogger(Servidor.class.getName()).log(Level.SEVERE, null, ex);
         }
